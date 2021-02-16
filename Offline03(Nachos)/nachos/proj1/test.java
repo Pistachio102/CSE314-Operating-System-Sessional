45dc5d865469115927e492dee16127bc2a1173da
Pistachio102 <tasnim282@gmail.com>
2021-02-16 23:57:10 +0600

    Nachos Offline Added

---
+++ Offline03(Nachos)/nachos/proj1/test.java
@@ -0,0 +1,10 @@
+package nachos.proj1;
+
+import nachos.threads.*;
+import nachos.machine.*;
+
+public class test{
+    public static void instantiateTest(){
+        System.out.println("hello there");
+    }
+}
